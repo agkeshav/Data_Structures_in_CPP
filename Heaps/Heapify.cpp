578bfa57caa09b79e7611d39026441d0c364c9cb
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2023-04-15 16:11:07 +0530

    Heapify

---
+++ Heaps/Heapify.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+class heap
+{
+    int arr[100];
+    int size;
+
+public:
+    heap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+};
+void heapify(int *arr, int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+    if (left < n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+    if (right < n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+int main()
+{
+    int arr[6] = {-1, 54, 53, 55, 52, 50};
+    int n = 5;
+    for (int i = n / 2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
