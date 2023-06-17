fbad5487569c13875f4dadf4695a9f5777e45484
agkeshav <keshav.aga12@gmail.com>
2023-06-17 14:10:07 +0530

    N Queens

---
+++ striver recursion/NQueens.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isSafe(int row, int col, vector<string> &board, int n)
+{
+    int x = row;
+    int y = col;
+
+    while (y >= 0)
+    {
+        if (board[x][y] == 'Q')
+            return false;
+        y--;
+    }
+
+    x = row;
+    y = col;
+
+    while (x >= 0 && y >= 0)
+    {
+        if (board[x][y] == 'Q')
+            return false;
+        x--;
+        y--;
+    }
+
+    x = row;
+    y = col;
+
+    while (x < n && y >= 0)
+    {
+        if (board[x][y] == 'Q')
+            return false;
+        x++;
+        y--;
+    }
+
+    return true;
+}
+void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n)
+{
+    if (col == n)
+    {
+        ans.push_back(board);
+        return;
+    }
+    for (int row = 0; row < n; row++)
+    {
+        if (isSafe(row, col, board, n))
+        {
+            board[row][col] = 'Q';
+            solve(col + 1, board, ans, n);
+            board[row][col] = '.';
+        }
+    }
+}
+vector<vector<string>> solveNQueens(int n)
+{
+    vector<vector<string>> ans;
+    vector<string> board(n, string(n, '.'));
+
+    int col = 0;
+    solve(col, board, ans, n);
+    return ans;
+}
+int main()
+{
+
+    return 0;
+}
