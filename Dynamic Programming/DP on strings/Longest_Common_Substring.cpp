db494b16147511aa7be6cc1cb4e307776e3b5898
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2023-07-03 14:24:31 +0530

    Longest Common Substring

---
+++ Dynamic Programming/DP on strings/Longest_Common_Substring.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+int lcs(string &str1, string &str2)
+{
+    //	Write your code here.
+    int n = str1.length();
+    int m = str2.length();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (str1[i - 1] == str2[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+                ans = max(ans, dp[i][j]);
+            }
+            else
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+    return ans;
+}
+
+// SPACE OPTIMISED
+int lcs(string &str1, string &str2)
+{
+    //	Write your code here.
+    int n = str1.length();
+    int m = str2.length();
+    vector<int> prev(m + 1, 0), curr(m + 1, 0);
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (str1[i - 1] == str2[j - 1])
+            {
+                curr[j] = 1 + prev[j - 1];
+                ans = max(ans, curr[j]);
+            }
+            else
+            {
+                curr[j] = 0;
+            }
+        }
+        prev = curr;
+    }
+    return ans;
+}
+int main()
+{
+
+    return 0;
+}
