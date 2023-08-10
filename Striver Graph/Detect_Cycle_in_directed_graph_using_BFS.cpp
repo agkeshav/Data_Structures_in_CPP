232e6859a1d9bbfb3b77ea4469b0a7335cf87ffb
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2023-08-10 19:26:42 +0530

    Detect Cycle in Directed Graph Using BFS

--- Striver Graph/Detect_Cycle_in_directed_graph_using_BFS.cpp
+++ Striver Graph/Detect_Cycle_in_directed_graph_using_BFS.cpp
@@ -1,53 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> topoSort(int V, vector<int> adj[])
+bool isCyclic(int V, vector<int> adj[])
 {
     // code here
-    vector<int> ans;
 
     vector<int> visited(V, 0);
     vector<int> indegree(V, 0);
+    int count = 0;
     for (int i = 0; i < V; i++)
     {
-        for (auto it : adj[i])
+        for (auto adjacentNode : adj[i])
         {
-            indegree[it]++;
+            indegree[adjacentNode]++;
         }
     }
-
     for (int i = 0; i < V; i++)
     {
-        if (visited[i] == 0)
+        if (!visited[i])
         {
+
             queue<int> q;
             for (int i = 0; i < V; i++)
             {
                 if (!visited[i] && indegree[i] == 0)
                 {
-                    visited[i] = 1;
                     q.push(i);
+                    visited[i] = 1;
                 }
             }
 
             while (q.size() > 0)
             {
-                int u = q.front();
-                ans.push_back(u);
+                int node = q.front();
                 q.pop();
-                for (auto it : adj[u])
+                count++;
+                for (auto adjacentNode : adj[node])
                 {
-                    indegree[it]--;
-                    if (!visited[it] && indegree[it] == 0)
+                    indegree[adjacentNode]--;
+                    if (indegree[adjacentNode] == 0 && visited[adjacentNode] == 0)
                     {
-                        q.push(it);
-                        visited[it] = 1;
+                        q.push(adjacentNode);
+                        visited[adjacentNode] = 1;
                     }
                 }
             }
         }
     }
-
-    return ans;
+    return (count != V);
 }
 int main()
 {
