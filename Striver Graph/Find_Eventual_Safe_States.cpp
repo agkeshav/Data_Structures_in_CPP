8920d1ed8f65234391636166d08cc2bef3e2a284
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2023-08-10 20:43:11 +0530

    Find Eventual Safe States using BFS and DFS

--- Striver Graph/Find_Eventual_Safe_States.cpp
+++ Striver Graph/Find_Eventual_Safe_States.cpp
@@ -46,6 +46,57 @@ vector<int> eventualSafeNodes(vector<vector<int>> &graph)
     }
     return ans;
 }
+
+
+// USING BFS
+vector<int> eventualSafeNodes(vector<vector<int>> &graph)
+{
+    vector<int> ans;
+    int v = graph.size();
+    vector<int> visited(v, 0);
+    vector<int> indegree(v, 0);
+    unordered_map<int, vector<int>> adj;
+    for (int i = 0; i < graph.size(); i++)
+    {
+        for (auto it : graph[i])
+        {
+            adj[it].push_back(i);
+        }
+    }
+    for (int i = 0; i < v; i++)
+    {
+        for (auto it : adj[i])
+        {
+            indegree[it]++;
+        }
+    }
+    queue<int> q;
+    for (int i = 0; i < v; i++)
+    {
+        if (indegree[i] == 0)
+        {
+            q.push(i);
+            visited[i] = 1;
+        }
+    }
+    while (q.size() > 0)
+    {
+        int u = q.front();
+        ans.push_back(u);
+        q.pop();
+        for (auto it : adj[u])
+        {
+            indegree[it]--;
+            if (!visited[it] && indegree[it] == 0)
+            {
+                q.push(it);
+                visited[it] = 1;
+            }
+        }
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
 int main()
 {
 
