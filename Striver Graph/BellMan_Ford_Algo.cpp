41886fa299b83324d8e54027efce13e6e2087ed6
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2023-08-18 20:23:38 +0530

    BellMan Ford Algorithm

---
+++ Striver Graph/BellMan_Ford_Algo.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> bellman_ford(int V, vector<vector<int>> &edges, int S)
+{
+    // Code here
+    vector<int> dist(V, 1e8);
+    dist[S] = 0;
+    for (int i = 0; i < V - 1; i++)
+    {
+        for (auto it : edges)
+        {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            if (dist[u] + wt < dist[v])
+            {
+                dist[v] = dist[u] + wt;
+            }
+        }
+    }
+    for (auto it : edges)
+    {
+        int u = it[0];
+        int v = it[1];
+        int wt = it[2];
+        if (dist[u] + wt < dist[v])
+        {
+            return {-1}; // negative cycle exists
+        }
+    }
+    return dist;
+}
+int main()
+{
+
+    return 0;
+}
