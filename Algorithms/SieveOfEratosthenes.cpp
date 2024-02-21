247e4e4b231f3480cbc6d08b72b0dd2b3c700534
Keshav Agarwal <99586399+agkeshav@users.noreply.github.com>
2024-02-21 21:45:36 +0530

    sieve modified

--- Algorithms/SieveOfEratosthenes.cpp
+++ Algorithms/SieveOfEratosthenes.cpp
@@ -9,7 +9,7 @@ int countPrimes(int n)
     int cnt = 0;
     vector<bool> prime(n + 1, true);
     prime[0] = prime[1] = false;
-    for (int i = 2; i < n; i++)
+    for (int i = 2; i*i < n; i++)
     {
         if (prime[i])
         {
